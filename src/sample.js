624d1bff8898e8cd90580917184a52f841bf2c39
DESKTOP-7TUJ74H\comin <yoursitup90@gmail.com>
2021-07-11 15:36:36 +0900

    git commit

---
+++ src/sample.js
@@ -0,0 +1,19 @@
+"use strict";
+function add(a, b) {
+    return a + b;
+}
+const sum = add(1, 2);
+console.log(sum); // 3
+let isBoolean;
+let isDone = false;
+// console.log(isBoolean);
+console.log(isDone);
+let num;
+let integer = 6;
+let float = 3.14;
+let hex = 0xf00d; // 61453
+let binary = 0b1010; // 10
+let octal = 0o744; // 484
+let infinity = Infinity;
+let nan = NaN;
+console.log(integer);
---
+++ src/sample.ts
@@ -0,0 +1,22 @@
+function add(a: number, b: number) {
+    return a + b;
+  }
+  const sum: number = add(1, 2);
+  console.log(sum); // 3
+
+let isBoolean: boolean;
+let isDone: boolean = false;
+
+// console.log(isBoolean);
+console.log(isDone);
+
+let num: number;
+let integer: number = 6;
+let float: number = 3.14;
+let hex: number = 0xf00d; // 61453
+let binary: number = 0b1010; // 10
+let octal: number = 0o744; // 484
+let infinity: number = Infinity;
+let nan: number = NaN;
+
+console.log(integer);
